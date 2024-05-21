be4048d0fc8c8ec39ddedf69865a792581dbba58
Aflalahmad <a.aflalahamed@gmail.com>
2024-05-21 12:17:09 +0530

    baseline virtual network

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+
+module "naming" {
+  source = "azure/naming/azurerm"
+  version = "0.4.1"
+  prefix = ["${var.environment}","${var.vnet_purpose}"]
+}
+
+
+
+
 resource "azurerm_virtual_network" "vnets" {
   name = var.vnet-name
-  resource_group_name = var.rg-details
+  resource_group_name = module.naming.resource_group.name
   location = var.location
   address_space = var.address_space
 }
@@ -10,7 +20,7 @@ resource "azurerm_subnet" "subnets" {
  for_each = var.subnet-name
   name                 = each.value
   address_prefixes     = [cidrsubnet(var.address_space[0], each.value.newbits,each.value.netnum)]
-  resource_group_name  = var.rg-details
+  resource_group_name  = module.naming.resource_group.name
   virtual_network_name = azurerm_virtual_network.vnets.name
   
 }
